18158ca9a88e226214e3b97a63f41c69160b1214
duran221 <cristianfernandoduran@gmail.com>
2019-04-04 23:36:28 -0500

    Algunos cambios :)

--- Sistemas_Operativos/Proyecto CHMaquina/pagina/javascript/proceso.js
+++ Sistemas_Operativos/Proyecto CHMaquina/pagina/javascript/proceso.js
@@ -1,9 +1,17 @@
 "use strict";
 
 export default class Proceso{
-    constructor(){
+
+    constructor(infoPrograma){
         console.log("hola");
+        //la lista almacena alguna informacion del programa.
         this.infoPrograma=[];
+        //Linea donde ha quedado el programa en ejecución antes de ser expropiado.
+        this.lineaEjecucion=0;
+        //Valor actual del acumulador al momento de ser expropiado
+        this.acumulador_actual;
+        
+
         
     }
 }
